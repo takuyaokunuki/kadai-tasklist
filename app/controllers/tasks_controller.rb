3081e440824c6f3a9c6467f2933033ec138bc554
takuya okunuki <taruikuchan@gmail.com>
2017-04-18 04:09:42 +0000

    tasklist DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    #@task = Task.find(params[:id])
   end
 
   def new
@@ -24,11 +26,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+   # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+  #  @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -40,7 +42,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-  @task = Task.find(params[:id])
+  #@task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -53,4 +55,8 @@ private
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
+  end
+  
+  def set_task
+    @task = Task.find(params[:id])
   end
