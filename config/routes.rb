4214cfd593c218858943c6b75f126c00817742b7
takuya okunuki <taruikuchan@gmail.com>
2017-04-20 02:20:53 +0000

    tasklist clonever

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'toppages/index'
 
-  resources :tasks
+  root to: 'toppages#index'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
